63865cb52c071b190e89654c2201f5fe8e8deee7
IngenieroEdu <jose.verdugo@cesunbc.edu.mx>
2026-03-26 14:49:06 -0700

    Delete Herramientas directory

--- Herramientas/get_direccion_mike.py
+++
@@ -1,8 +0,0 @@
-from geopy.geocoders import Nominatim
-
-def get_direccion_mikecore(lat: str, lon: str):
-    geolocator = Nominatim(user_agent="mike")
-    location = geolocator.reverse(f"{lat}, {lon}")
-    return location.address
-
-
